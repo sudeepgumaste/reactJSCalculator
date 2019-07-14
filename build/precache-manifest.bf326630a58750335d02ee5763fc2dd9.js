a174960f92ed2dae26232c91486bb27b2146b223
Sudeep G <sudeepgumaste@github.com>
2019-07-14 23:50:02 +0530

    removed user scalablity

---
+++ build/precache-manifest.bf326630a58750335d02ee5763fc2dd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c34ece97754fc1dc85f68347ba6067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6bde357daff678c29c4",
+    "url": "/static/css/main.c93ee183.chunk.css"
+  },
+  {
+    "revision": "7b3a90694b8ced4264ee",
+    "url": "/static/js/2.6e7c3acd.chunk.js"
+  },
+  {
+    "revision": "d6bde357daff678c29c4",
+    "url": "/static/js/main.3d7a3d4f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
