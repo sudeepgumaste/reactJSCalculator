78368aeb436fbfeb1bc9699120b97d40ae439d3f
Sudeep G <sudeepgumaste@github.com>
2019-07-14 18:02:04 +0530

    removed vulnerable dependencies, ceinitialised app with create-react-app

---
+++ README.md
@@ -0,0 +1,33 @@
+# ReactJS calculator
+
+A simple react js calculator.
+
+![Screenshot](./calc.png)
+
+## How to run / build 
+Installing the packages
+```bash
+yarn  install
+```
+or
+```bash
+npm  install
+```
+
+Starting the development server
+```bash
+yarn start
+```
+or
+```bash
+npm start
+```
+
+Building the app
+```bash
+yarn build
+```
+or
+```bash
+npm run-script build
+```
---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import {evaluate} from 'mathjs';
+import './App.css'
+
+const Screen = (props) =>{
+  return(
+      <div className="screen">
+          <p>{props.dispValue}</p>
+      </div>
+  );
+}
+
+class Button extends React.Component{
+  constructor(props){
+      super(props);
+      this.onButtonPress = this.onButtonPress.bind(this);
+  }
+
+  onButtonPress(){
+      this.props.onButtonPress(this.props.value);
+  }
+
+  render(){
+      return(
+          <button onClick={this.onButtonPress}>{this.props.value}</button>
+      );
+  }
+}
+
+
+export default class Calculator extends React.Component{
+  constructor(props){
+      super(props);
+      this.onButtonPress = this.onButtonPress.bind(this);
+      this.clrBtn = this.clrBtn.bind(this);
+      this.delBack = this.delBack.bind(this);
+      this.calcRes = this.calcRes.bind(this);
+      this.state = {
+          scrStr : ''
+      };
+  }
+
+  onButtonPress(btnVal){
+      this.setState((prevState)=>{
+          return{
+              scrStr : prevState.scrStr + btnVal
+          };
+      });
+  }
+
+  clrBtn(){
+      this.setState(()=>{
+          return{
+              scrStr : ''
+          };
+      });
+  }
+
+  delBack(){
+      this.setState((prevState) =>{
+          return{
+              scrStr : prevState.scrStr.substring(0,prevState.scrStr.length-1)
+          };
+      });
+  }
+
+  calcRes(){
+      this.setState((prevState) =>{
+          if(prevState.scrStr){
+              return{
+                  scrStr : String(evaluate(prevState.scrStr))
+              };
+          }
+      });
+  }
+
+  render(){
+      return(
+          <div className="container">
+              <Screen dispValue={this.state.scrStr}/>
+              <button onClick={this.clrBtn}>C</button>
+              <Button value={'%'} onButtonPress={this.onButtonPress}/>
+              <button onClick = {this.calcRes}>=</button>
+              <button onClick = {this.delBack}><i className="fas fa-backspace"></i></button>
+              {[7,8,9].map((num)=>(<Button value={num} onButtonPress={this.onButtonPress}/>))}
+              <Button value={'+'} onButtonPress={this.onButtonPress}/>
+              {[4,5,6].map((num)=>(<Button value={num} onButtonPress={this.onButtonPress}/>))}
+              <Button value={'-'} onButtonPress={this.onButtonPress}/>
+              {[1,2,3].map((num)=>(<Button value={num} onButtonPress={this.onButtonPress}/>))}
+              <Button value={'*'} onButtonPress={this.onButtonPress}/>
+              <Button value={0} onButtonPress={this.onButtonPress}/>
+              <Button value={'.'} onButtonPress={this.onButtonPress}/>
+              <Button value={'/'} onButtonPress={this.onButtonPress}/>
+          </div>
+      )
+  }
+}
